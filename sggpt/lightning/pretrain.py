fix wrong indexing in contrastive loss calculation

sggpt/lightning/pretrain.py
@@ -63,7 +63,7 @@ class PretrainSelfGenomeGPT(LightningModule):
             "collate_fn": self.collator.batch,
         }
 
-    def forward(self, x):
+    def forward(self, x: torch.LongTensor):
         out = self.model.forward(x)
         normed_logits = nn.functional.normalize(out.head_logits, dim=-1)
         foward_strand_logits, reverse_strand_logits = torch.chunk(
@@ -73,22 +73,52 @@ class PretrainSelfGenomeGPT(LightningModule):
         reverse_strand_logits = rearrange(reverse_strand_logits, "b l c -> l c b")
         return foward_strand_logits, reverse_strand_logits
 
-    def contrastive_loss(self, f, b):
-        max_len = f.shape[0]  # e.g. 8
+    def contrastive_loss(self, f: torch.Tensor, b: torch.Tensor):
+        """
+        f: (seq_len, batch, hidden)
+        b: (seq_len, hidden, batch)
+        """
+        transformer_max_len = f.shape[0]  # e.g. 8
         batch_size = f.shape[1]
-        f = f[:-2]  # e.g. (012345)67
-        # b is        e.g. 76(543210)
 
-        l = f.shape[0]
+        ## if original seq is:
+        #   ABCDEFGH
+        ## seqlen = 8 and conv size = 2
+        ## and the transformer_seqlen will be (8 / (2 / 2)) - 1 = 7
 
-        b_idx = torch.arange(f.shape[0], device=f.device) + 2
-        b_idx = (max_len - 1) - b_idx
-        b = b[b_idx]
+        ## then the forward stran on the seqlen dim will get the information of:
+        #   0  1  2  3  4  5  6
+        #   AB BC CD DE EF FG GH
 
-        logits = torch.bmm(f, b)  # (max len - 2, batch, batch)
+        ## and the reverse strand will get the information of: (- means reverse complement)
+        #   0   1   2   3   4   5   6
+        #   -HG -GF -FE -ED -DC -CB -BA
+
+        ## so the embedding we want to contrast is those not leaked by the conv size,
+        ## but can compose to the original seq
+        ## for example, if we take the forward[2, CD] element, the target (non overlapped)
+        ## should be the reverse strand of forward[4, EF] (which is 2 + 2) element
+        ## so the target should be reverse[2, -FE]
+
+        ## based on the above derivation, we know the Fsrc-Rtarget index formula is:
+        ## idx_reverse = (transformer_seqlen - 1) - (idx_forward + 2)
+        ##             = (transformer_seqlen - 3) - idx_forward
+        ##             = (7 - 3) - 2 = 2
+
+        f = f[:-2]
+        valid_l = f.shape[0]
+
+        f_idx = torch.arange(valid_l, device=f.device)
+
+        b_idx = (transformer_max_len - 3) - f_idx
+        b = b[b_idx]  # re-index the reverse strand
+
+        logits = torch.bmm(f, b)  # (valid_l, batch, batch)
         logits = rearrange(logits, "l a b -> (l a) b")
 
-        target = repeat(torch.arange(batch_size, device=f.device), "b -> (l b)", l=l)
+        target = repeat(
+            torch.arange(batch_size, device=f.device), "b -> (l b)", l=valid_l
+        )
         loss = self.loss_fn(
             logits,
             target,

sggpt/models/sggpt.py
@@ -28,6 +28,18 @@ class SelfGenomeGPTConfig:
     mlp_dropout: float = 0.0
     attn_impl: str = "flash"  # 'sdpa' or 'flash' or 'slow'
 
+    @property
+    def stride(self):
+        return self.conv_size // 2
+
+    @property
+    def transformer_max_len(self):
+        """max_len is the input DNA sequence max length
+        but transformer_max_len is the max length of the transformer input
+            (which is the max len after conv1d)
+        """
+        return (self.max_len // self.stride) - 1
+
     def __post_init__(self):
         assert self.d_model % self.n_heads == 0, "d_model must be divisible by n_heads"
         assert self.initializer_range > 0.0, "initializer_range must be positive"
@@ -178,15 +190,14 @@ class SelfGenomeGPTPositionalEncoding(nn.Module):
         super().__init__()
         self.config = config
         d_model = config.d_model
-        self.stride = config.conv_size // 2
-        max_len = config.max_len // self.stride
+        self.transformer_max_len = config.transformer_max_len
 
         self.register_buffer(
             "position_ids",
-            torch.arange(max_len).expand((1, -1)),
+            torch.arange(self.transformer_max_len).expand((1, -1)),
             persistent=False,
         )
-        self.position_embeddings = nn.Embedding(max_len, d_model)
+        self.position_embeddings = nn.Embedding(self.transformer_max_len, d_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -210,19 +221,24 @@ class SelfGenomeGPT(nn.Module):
     def __init__(self, config: SelfGenomeGPTConfig):
         super().__init__()
         self.config = config
-        self.stride = config.conv_size // 2
+        self.stride = config.stride
+        self.transformer_max_len = config.transformer_max_len
+
         self.in_conv = nn.Conv1d(
             in_channels=4,
             out_channels=config.d_model,
             kernel_size=config.conv_size,
             stride=self.stride,
-            padding=self.stride,
         )
+
         self.pos_encoding = SelfGenomeGPTPositionalEncoding(config)
+
         self.layers = nn.ModuleList(
             [SelfGenomeGPTLayer(config) for _ in range(config.n_layers)]
         )
+
         self.out_norm = nn.LayerNorm(config.d_model, eps=config.norm_eps)
+
         self.head = SelfGenomeGPTMLPHead(config)
 
     def forward(self, x: torch.LongTensor) -> SelfGenomeGPTOutput:
@@ -234,12 +250,11 @@ class SelfGenomeGPT(nn.Module):
         Returns:
             SelfGenomeGPTOutput: Output of the model, including hidden states and head logits
         """
-        bs, seqlen = x.shape
-        seqlen_for_transformer = seqlen // self.stride
-
         x_onehot = F.one_hot(x, num_classes=4)
         x_input = rearrange(x_onehot, "b l c -> b c l").float()  # for conv1d
-        x_input = self.in_conv(x_input)[:, :, 1:]
+
+        # FIXME: this part might be wrong, need to check grad
+        x_input = self.in_conv(x_input)
         x_input = rearrange(
             x_input, "b c l -> b l c"
         )  # back to (batch_size, seq_len, d_model)